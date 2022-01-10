203b580efe4303d8fe1051b67728b458d67cb7bb
Srabanti Guha <87993392+srabantiguha@users.noreply.github.com>
2022-01-09 18:57:36 -0700

    first commit

---
+++ split-data.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Usage:
+    split-data.py [-p partitions] [-o output] DATASET
+
+Options:  
+    -p partitions     number of cross-folds [default: 5]
+    -o output         destination directory [default: data-split]
+    DATASET           name of data set to load  
+"""
+
+from docopt import docopt
+from lkdemo import datasets, log
+from pathlib import Path
+
+import lenskit.crossfold as xf
+
+def main(args):
+    dsname = args.get('DATASET')
+    partitions = int(args.get('-p'))
+    output = args.get('-o')
+
+    _log.info('locating data set %s', dsname)
+    data = getattr(datasets, dsname)
+
+    _log.info('loading ratings')
+    ratings = data.ratings
+
+    path = Path(output)
+    path.mkdir(exist_ok=True, parents=True)
+
+    _log.info('writing to %s', path)
+    testRowsPerUsers = 5
+    for i, tp in enumerate(xf.partition_users(ratings, partitions, xf.SampleN(testRowsPerUsers)), 1):
+        # _log.info('writing train set %d', i)
+        # tp.train.to_csv(path / f'train-{i}.csv.gz', index=False)
+        _log.info('writing test set %d', i)
+        tp.test.index.name = 'index'
+        tp.test.to_csv(path / f'test-{i}.csv.gz')
+        
+
+if __name__ == '__main__':
+    _log = log.script(__file__)
+    args = docopt(__doc__)
+    main(args)
---
+++ train-model.py
@@ -0,0 +1,65 @@
+"""
+Train a recommendation model.
+
+Usage:
+  train-model.py [-m MODULE] [-o FILE] ALGO DATASET
+
+Options:
+  -m MODULE     import algorithms from MODULE [default: lkdemo.algorithms]
+  -o FILE       write trained model to FILE
+  ALGO          name of algorithm to load
+  DATASET       name of data set to load
+"""
+
+from docopt import docopt
+import pathlib
+import importlib
+import gzip
+import pickle
+try:
+    import resource
+except ImportError:
+    resource = None
+
+from lenskit.util import Stopwatch
+from lenskit.algorithms import Recommender
+from lkdemo import datasets, log
+
+def main(args):
+    mod_name = args.get('-m', 'lkdemo.algorithms')
+    out = args.get('FILE', None)
+    model = args.get('ALGO')
+    dsname = args.get('DATASET')
+
+    _log.info('importing from module %s', mod_name)
+    algorithms = importlib.import_module(mod_name)
+
+    _log.info('locating model %s', model)
+    algo = getattr(algorithms, model)
+    _log.info('locating data set %s', dsname)
+    data = getattr(datasets, dsname)
+
+    _log.info('loading ratings')
+    ratings = data.ratings
+    _log.info('training model')
+    algo = Recommender.adapt(algo)
+    timer = Stopwatch()
+    algo.fit(ratings)
+    timer.stop()
+    _log.info('trained model in %s', timer)
+    if resource:
+        res = resource.getrusage(resource.RUSAGE_SELF)
+        _log.info('%.2fs user, %.2fs system, %.1fMB max RSS', res.ru_utime, res.ru_stime, res.ru_maxrss / 1024)
+
+    if out is None:
+        out = f'models/{dsname}-{model}.pkl.gz'
+
+    _log.info('writing to %s', out)
+    pathlib.Path(out).parent.mkdir(parents=True, exist_ok=True)
+    with gzip.open(out, 'wb') as f:
+        pickle.dump(algo, f, 4)
+
+if __name__ == '__main__':
+    _log = log.script(__file__)
+    args = docopt(__doc__)
+    main(args)
